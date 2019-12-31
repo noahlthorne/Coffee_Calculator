ec45c36f7dd781c2f301363e98e73da47f7875a0
Noah Thorne <noahlthorne@gmail.com>
2019-12-31 11:53:18 +0900

    added brewer seeds and made a brewer index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Removing brew methods..."
+Brewer.destroy_all
+puts "Creating new brew methods..."
+Brewer.create!(category: "Pour Over")
+Brewer.create!(category: "Espresso")
+Brewer.create!(category: "Moka Pot")
+Brewer.create!(category: "Aeropress")
+Brewer.create!(category: "French Press")
+Brewer.create!(category: "Soft Brew")
+Brewer.create!(category: "Cold Brew")
+puts "Created #{Brewer.count} brewing methods"
