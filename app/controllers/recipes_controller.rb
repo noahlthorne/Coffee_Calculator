6d3a9b00f6a8cf59cad52a2b88c5e9eb21d28b09
Noah Thorne <noahlthorne@gmail.com>
2020-01-06 16:00:20 +0900

    added recipe create controller action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,6 @@
 class RecipesController < ApplicationController
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   resources :brewers, only: [:index]
   resources :coffees, only: [:index]
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :create]
 end
