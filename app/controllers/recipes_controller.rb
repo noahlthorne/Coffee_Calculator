fa06b25a178dc95ac5735d83e7bdfa2eb40db96a
Noah Thorne <noahlthorne@gmail.com>
2020-01-10 21:12:21 +0900

    added recipe show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,8 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_params)
     @recipe.save
   end
+
+  def show
+    @recipe = recipe.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   resources :brewers, only: [:index]
   resources :coffees, only: [:index]
-  resources :recipes, only: [:index, :create]
+  resources :recipes, only: [:index, :create, :show]
 end
