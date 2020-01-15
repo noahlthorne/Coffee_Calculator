a5447cd8cea566387a73eabe72bd70f30dadb5eb
Noah Thorne <noahlthorne@gmail.com>
2020-01-15 17:23:51 +0900

    re-created tables

--- db/migrate/20200114054233_remove_origin_from_coffees.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOriginFromCoffees < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :coffees, :origin, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_054350) do
+ActiveRecord::Schema.define(version: 2020_01_14_053809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 2020_01_14_054350) do
 
   create_table "coffees", force: :cascade do |t|
     t.string "roast"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "origin", force: :cascade do |t|
-    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
