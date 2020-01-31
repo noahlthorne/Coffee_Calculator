575e99433246d2c6f3185d68f0cb6044e4ade17a
Noah Thorne <noahlthorne@gmail.com>
2020-01-31 16:29:52 +0900

    changed home to brewers#index

--- app/controllers/brewers_controller.rb
+++ app/controllers/brewers_controller.rb
@@ -1,4 +1,11 @@
 class BrewersController < ApplicationController
+  def home
+  end
+
+  def index
+    @brewers = Brewer.all
+  end
+
   def show
     @brewer = Brewer.find(params[:id])
     @coffee = Coffee.find(params[coffee_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
+  root to: 'brewers#index'
   resources :brewers, only: [:index, :show]
   resources :coffees, only: [:index]
   resources :recipes, only: [:index, :create, :show]
